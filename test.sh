c55b24c35eb3f7a1456ac8c91f8b4c528522f11d
yutarofire <yutaro.ono.418@gmail.com>
2020-04-19 16:00:18 +0900

    Improve test

--- test.sh
+++ test.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
-set -e
-
 cc -o 9cc 9cc.c
-./9cc 123 > 9cc.s
-cat 9cc.s
 
-rm 9cc 9cc.s
+assert() {
+  expected=$1
+  input=$2
+
+  ./9cc "$input" > tmp.s
+  cc -o tmp tmp.s
+  ./tmp
+  actual=$?
+  rm tmp tmp.s
+
+  if [ "$actual" = "$expected" ]; then
+    echo "$input => $actual"
+  else
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+assert 0 0
+assert 42 42
+
+rm 9cc
+
+echo OK
