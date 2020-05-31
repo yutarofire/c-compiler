7cd99fae97ebcac2511d3550cfca8b008558ea2f
yutarofire <yutaro.ono.418@gmail.com>
2020-05-31 19:36:11 +0900

    Add test case for sizeof of array

--- test.sh
+++ test.sh
@@ -128,5 +128,6 @@ assert 3 'int main() { int a[3]; *a=1; *(a+1)=2; *(a+2)=3; return *(a+2); }'
 assert 1 'int main() { int a[3]; a[0]=1; a[1]=2; a[2]=3; return a[0]; }'
 assert 2 'int main() { int a[3]; a[0]=1; a[1]=2; a[2]=3; return a[1]; }'
 assert 3 'int main() { int a[3]; a[0]=1; a[1]=2; a[2]=3; return a[2]; }'
+assert 24 'int main() { int a[3]; return sizeof(a); }'
 
 echo OK
